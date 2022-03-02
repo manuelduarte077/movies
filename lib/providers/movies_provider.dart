572367c869b2180a0c16d25c57f31835518d29cd
Manuel Duarte <carlosd99m@gmail.com>
2022-03-02 17:09:20 -0600

    feat: mapeando la peticion (http)

--- lib/providers/movies_provider.dart
+++ lib/providers/movies_provider.dart
@@ -1,13 +1,29 @@
 import 'package:flutter/material.dart';
 
+import 'package:http/http.dart' as http;
+import 'package:movies/models/now_playing.dart';
+
 class MoviesProvider extends ChangeNotifier {
+  final String _apiKey = '72c3440db6b927508f12139e55f90a26';
+  final String _baseUrl = 'api.themoviedb.org';
+  final String _language = 'en-ES';
+
   MoviesProvider() {
     print("MoviesProvider()");
 
-    this.getOnDisplayMovies();
+    getOnDisplayMovies();
   }
 
   getOnDisplayMovies() async {
-    print("getOnDisplayMovies");
+    var url = Uri.https(_baseUrl, '/3/movie/now_playing', {
+      'api_key': _apiKey,
+      'language': _language,
+      'page': '1',
+    });
+
+    var response = await http.get(url);
+    final nowPlayingResponse = NowPlayingResponse.fromJson(response.body);
+
+    print("response.body: ${nowPlayingResponse.results[1].title}");
   }
 }
