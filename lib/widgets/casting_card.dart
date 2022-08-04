239e4daf2088984e03fae30eeb021cdd2c90e5eb
manuelduarte077 <carlosd99m@gmail.com>
2022-08-04 01:21:45 -0600

    showing the casting

--- lib/widgets/casting_card.dart
+++ lib/widgets/casting_card.dart
@@ -1,25 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:movies/models/model.dart';
+import 'package:movies/providers/movies_provider.dart';
+import 'package:provider/provider.dart';
 
 class CastingCardWidget extends StatelessWidget {
-  const CastingCardWidget({super.key});
+  const CastingCardWidget({super.key, required this.movieId});
+
+  final int movieId;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.only(bottom: 30),
-      width: double.infinity,
-      height: 180,
-      child: ListView.builder(
-        scrollDirection: Axis.horizontal,
-        itemCount: 5,
-        itemBuilder: (context, index) => const _CastCard(),
-      ),
+    final moviesProvider = Provider.of<MoviesProvider>(context, listen: false);
+
+    return FutureBuilder(
+      future: moviesProvider.getMoviesCasting(movieId),
+      builder: (_, AsyncSnapshot<List<Cast>> snapshot) {
+        if (!snapshot.hasData) {
+          return Container(
+            constraints: const BoxConstraints(maxWidth: 150),
+            height: 180,
+            child: const CircularProgressIndicator.adaptive(),
+          );
+        }
+
+        final List<Cast> cast = snapshot.data!;
+
+        return Container(
+          margin: const EdgeInsets.only(bottom: 30),
+          width: double.infinity,
+          height: 180,
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: cast.length,
+            itemBuilder: (context, index) => _CastCard(
+              cast[index],
+            ),
+          ),
+        );
+      },
     );
   }
 }
 
 class _CastCard extends StatelessWidget {
-  const _CastCard();
+  final Cast actor;
+
+  const _CastCard(this.actor);
 
   @override
   Widget build(BuildContext context) {
@@ -31,21 +57,21 @@ class _CastCard extends StatelessWidget {
         children: [
           ClipRRect(
             borderRadius: BorderRadius.circular(10),
-            child: const FadeInImage(
-              image: NetworkImage('https://picsum.photos/300/200?random'),
-              placeholder: AssetImage('assets/loading.gif'),
+            child: FadeInImage(
+              image: NetworkImage(actor.fullProfilePath),
+              placeholder: const AssetImage('assets/loading.gif'),
               height: 140,
               width: 110,
               fit: BoxFit.cover,
             ),
           ),
           const SizedBox(height: 5),
-          const Text(
-            'actor.name, cast name',
+          Text(
+            actor.name,
             maxLines: 2,
             overflow: TextOverflow.ellipsis,
             textAlign: TextAlign.center,
-            style: TextStyle(
+            style: const TextStyle(
               fontSize: 12,
               fontWeight: FontWeight.w600,
             ),
